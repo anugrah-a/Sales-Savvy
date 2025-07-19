5b1110d872c9c1c800f75be1dbaad58652ae3df4
anugrah-a <anugrahanoop29@gmail.com>
2025-07-19 23:01:24 +0530

    test

--- SalesSavvy/src/main/java/com/salesSavvy/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesSavvy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesSavvy.entity.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders, String>{
-
-}
