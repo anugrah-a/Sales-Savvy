b549ac6d4303fdd4f123f00a7e29f8e2ff542f56
anugrah-a <anugrahanoop29@gmail.com>
2025-07-20 13:40:07 +0530

    deleted folder inside local repo

--- SalesSavvy-BackEnd/src/main/java/com/salesSavvy/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesSavvy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesSavvy.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
